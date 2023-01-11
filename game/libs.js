d76c9ca4436d6d85da2419a466c68ead9de8e254
thaysang <104902053+thaysang@users.noreply.github.com>
2023-01-11 15:34:33 +0000

    v03

--- game/libs.js
+++ game/libs.js
@@ -12,27 +12,20 @@ class Sprite extends PIXI.Sprite {
   }
 }
 
-const sprite = (texture,{x=0,y=0,anchor=0.5,scale=1}={}) => {
+const sprite = (texture,{x=0,y=0,anchor=0.5,scale=1,...props}={}) => {
   const sp = new Sprite(texture)
   sp.interactive = true;
   sp.anchor.set(anchor)
   sp.x = x
   sp.y = y
   sp.scale.set(scale)
+  Object.assign(sp,props)
   app.stage.addChild(sp);
   return sp
 }
 
 const image = (url) => PIXI.Texture.from(url)
 
-const circle = (r,{fill="black",borderWidth=0,borderColor="black"}={}) => {
-  graphic.clear() 
-  graphic.lineStyle(borderWidth,_color(borderColor),1); 
-  graphic.beginFill(_color(fill));
-  graphic.drawCircle(0,0,r);
-  graphic.endFill();
-  return app.renderer.generateTexture(graphic)
-}
 const rect = (width,height,{fill="black",lineWidth=0,lineColor="black",borderRadius=0}={}) => {
   graphic.clear() 
   graphic.lineStyle(lineWidth,_color(lineColor),1); 
@@ -42,7 +35,14 @@ const rect = (width,height,{fill="black",lineWidth=0,lineColor="black",borderRad
   graphic.endFill();
   return app.renderer.generateTexture(graphic)
 }
-
+const circle = (r,{fill="black",borderWidth=0,borderColor="black"}={}) => {
+  graphic.clear() 
+  graphic.lineStyle(borderWidth,_color(borderColor),1); 
+  graphic.beginFill(_color(fill));
+  graphic.drawCircle(0,0,r);
+  graphic.endFill();
+  return app.renderer.generateTexture(graphic)
+}
 const star = (n,width,{fill="black",lineWidth=0,lineColor="black"}={}) => {
   graphic.clear() 
   graphic.lineStyle(lineWidth,_color(lineColor),1); 
@@ -51,7 +51,6 @@ const star = (n,width,{fill="black",lineWidth=0,lineColor="black"}={}) => {
   graphic.endFill();
   return app.renderer.generateTexture(graphic)
 }
-
 const elip = (width,height,{fill="black",lineWidth=0,lineColor="black"}={}) => {
   graphic.clear() 
   graphic.lineStyle(lineWidth,_color(lineColor),1); 
@@ -60,7 +59,6 @@ const elip = (width,height,{fill="black",lineWidth=0,lineColor="black"}={}) => {
   graphic.endFill();
   return app.renderer.generateTexture(graphic)
 }
-
 const polygon = (path,{fill="black",lineWidth=0,lineColor="black"}={}) => {
   graphic.clear() 
   graphic.lineStyle(lineWidth,_color(lineColor),1); 
@@ -77,4 +75,18 @@ const text = (txt,{x=0,y=0,...style}={}) => {
   basicText.anchor.set(0.5)
   app.stage.addChild(basicText);
   return basicText
+}
+//Sounds
+const sound = {
+  add: (name) => PIXI.sound.add(name,'/sound/'+name),
+  play: (name) => PIXI.sound.play(name)
+}
+
+const keyboard = {
+  down: (cb) => {
+    window.addEventListener('keydown',(e)=>cb(e.key))
+  },
+  up: (cb) => {
+    window.addEventListener('keyup',(e)=>cb(e.key))
+  }
 }
