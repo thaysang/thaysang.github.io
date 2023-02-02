92fac7746eec791e262ca09706ea551a29ac5595
thaysang <104902053+thaysang@users.noreply.github.com>
2023-02-02 14:13:09 +0000

    tile

---
+++ minigame/tile/tile.js
@@ -0,0 +1,39 @@
+const bg = [
+    "BG.png"
+]
+const object = [
+    "Bush (1).png",
+    "Bush (2).png",
+    "Bush (3).png",
+    "Bush (4).png",
+    "Crate.png",
+    "Mushroom_1.png",
+    "Mushroom_2.png",
+    "Sign_1.png",
+    "Sign_2.png",
+    "Stone.png",
+    "Tree_1.png",
+    "Tree_2.png",
+    "Tree_3.png",    
+]
+const tile = [
+    "1.png",
+    "10.png",
+    "11.png",
+    "12.png",
+    "13.png",
+    "14.png",
+    "15.png",
+    "16.png",
+    "17.png",
+    "18.png",
+    "2.png",
+    "3.png",
+    "4.png",
+    "5.png",
+    "6.png",
+    "7.png",
+    "8.png",
+    "9.png",    
+]
+export default {bg,object,tile}
