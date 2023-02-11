b448cf18964b006d15dbdaabd08c62125e6641f6
thaysang <104902053+thaysang@users.noreply.github.com>
2023-02-11 12:59:01 +0000

    new_turtle

---
+++ turtle/turtle_libs.js
@@ -0,0 +1,51 @@
+turtle.setIcon("")
+turtle.setSize(30)
+turtle.setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvTiOI6jPCmwVAId1q0rCafG1WXcbJjx4tRg&usqp=CAU")
+
+const fd = (p) => turtle.fd(p)
+const forward = (p) => turtle.fd(p)
+
+const bk = (p) => turtle.bk(p)
+const backward = (p) => turtle.bk(p)
+const back = (p) => turtle.bk(p)
+
+const lt = (p) => turtle.lt(p)
+const left = (p) => turtle.lt(p)
+
+const rt = (p) => turtle.rt(p)
+const right = (p) => turtle.rt(p)
+
+const gt = (x,y) => turtle.gt(240+x,180-y)
+
+const seth = (p) => turtle.seth(p)
+
+const bf = (p) => turtle.bf(p)
+const beginFill = (p) => turtle.bf(p)
+
+const ps = (p) => turtle.ps(p)
+const pc = (p) => turtle.pc(p)
+
+const speed = (p) => turtle.speed(p)
+const text = (p,cl="black",s=22,f="Arial") => {
+    turtle.setFont(s+"px "+f)
+    turtle.setFillStyle(cl)
+    turtle.fillText(p)
+}
+
+const ef = () => turtle.ef()
+const endFill = () => turtle.ef()
+
+const home = () => turtle.home()
+const clear = () => turtle.clear()
+const pd = () => turtle.pd()
+const penDown = () => turtle.pd()
+const hide = () => turtle.setIcon("")
+const icon = (p) => turtle.setIcon(p)
+
+const pu = () => turtle.pu()
+const penUp = () => turtle.pu()
+
+const circle = (r,a=360,c=true) => turtle.circle(r,a,c)
+const cc = (r,a=360,c=true) => turtle.circle(r,a,c)
+
+seth(90)
