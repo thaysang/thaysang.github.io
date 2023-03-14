8695329d43284e3230bbe1e5bebcbae62731b318
thaysang <104902053+thaysang@users.noreply.github.com>
2023-03-14 13:24:08 +0000

    jumpto

--- turtle/turtle_libs.js
+++ turtle/turtle_libs.js
@@ -16,8 +16,8 @@ const rt = (p) => turtle.rt(p)
 const right = (p) => turtle.rt(p)
 
 const gt = (x,y) => turtle.gt(240+x,180-y)
-const mt = (x,y) => turtle.setPosition(240+x,180-y)
-const moveTo = (x,y) => turtle.setPosition(240+x,180-y)
+const jt = (x,y) => turtle.setPosition(240+x,180-y)
+const jumpTo = (x,y) => turtle.setPosition(240+x,180-y)
 const pos = (x,y) => turtle.setPosition(240+x,180-y)
 
 const seth = (p) => turtle.seth(p)
