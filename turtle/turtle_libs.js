c269bd3b93dcbb8f7142230edf0f89a6d3771dc1
thaysang <104902053+thaysang@users.noreply.github.com>
2023-03-16 04:14:52 +0000

    default

--- turtle/turtle_libs.js
+++ turtle/turtle_libs.js
@@ -2,38 +2,38 @@ turtle.setIcon("")
 turtle.setSize(30)
 turtle.setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvTiOI6jPCmwVAId1q0rCafG1WXcbJjx4tRg&usqp=CAU")
 
-const fd = (p) => turtle.fd(p)
-const forward = (p) => turtle.fd(p)
+const fd = (p=100) => turtle.fd(p)
+const forward = (p=100) => turtle.fd(p)
 
-const bk = (p) => turtle.bk(p)
-const backward = (p) => turtle.bk(p)
-const back = (p) => turtle.bk(p)
+const bk = (p=100) => turtle.bk(p)
+const backward = (p=100) => turtle.bk(p)
+const back = (p=100) => turtle.bk(p)
 
-const lt = (p) => turtle.lt(p)
-const left = (p) => turtle.lt(p)
+const lt = (p=90) => turtle.lt(p)
+const left = (p=90) => turtle.lt(p)
 
-const rt = (p) => turtle.rt(p)
-const right = (p) => turtle.rt(p)
+const rt = (p=90) => turtle.rt(p)
+const right = (p=90) => turtle.rt(p)
 
-const gt = (x,y) => turtle.gt(240+x,180-y)
-const goTo = (x,y) => turtle.gt(240+x,180-y)
-const jt = (x,y) => turtle.setPosition(240+x,180-y)
-const jumpTo = (x,y) => turtle.setPosition(240+x,180-y)
-const pos = (x,y) => turtle.setPosition(240+x,180-y)
+const gt = (x=0,y=0) => turtle.gt(240+x,180-y)
+const goTo = (x=0,y=0) => turtle.gt(240+x,180-y)
+const jt = (x=0,y=0) => turtle.setPosition(240+x,180-y)
+const jumpTo = (x=0,y=0) => turtle.setPosition(240+x,180-y)
+const pos = (x=0,y=0) => turtle.setPosition(240+x,180-y)
 
-const seth = (p) => turtle.seth(p)
+const seth = (p=0) => turtle.seth(p)
 
-const bf = (p) => turtle.bf(p)
-const beginFill = (p) => turtle.bf(p)
+const bf = (p="black") => turtle.bf(p)
+const beginFill = (p="black") => turtle.bf(p)
 
-const ps = (p) => turtle.ps(p)
-const penSize = (p) => turtle.ps(p)
+const ps = (p=1) => turtle.ps(p)
+const penSize = (p=1) => turtle.ps(p)
 
-const pc = (p) => turtle.pc(p)
-const penColor = (p) => turtle.pc(p)
+const pc = (p="black") => turtle.pc(p)
+const penColor = (p="black") => turtle.pc(p)
 
-const speed = (p) => turtle.speed(p)
-const text = (p,cl="black",s=22,f="Arial") => {
+const speed = (p=1) => turtle.speed(p)
+const text = (p="",cl="black",s=22,f="Arial") => {
     turtle.setFont(s+"px "+f)
     turtle.setFillStyle(cl)
     turtle.fillText(p)
@@ -56,6 +56,6 @@ const penUp = () => turtle.pu()
 
 const circle = (r,a=360,c=true) => turtle.circle(r,a,c)
 const cc = (r,a=360,c=true) => turtle.circle(r,a,c)
-const dot = (size,color) => turtle.dot(size,color)
+const dot = (size=20,color="black") => turtle.dot(size,color)
 
 seth(90)
